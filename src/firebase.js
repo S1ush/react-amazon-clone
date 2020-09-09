3b3b19e4c063a6bb55f4ddad093c5d1bd5ec1065
S1ush <37140143+S1ush@users.noreply.github.com>
2020-09-09 16:45:18 +0530

    init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCzD7lwC_TawOrSTVQTVGD__FmeZfpJ9Jc",
+    authDomain: "clone-8ac8f.firebaseapp.com",
+    databaseURL: "https://clone-8ac8f.firebaseio.com",
+    projectId: "clone-8ac8f",
+    storageBucket: "clone-8ac8f.appspot.com",
+    messagingSenderId: "333307118863",
+    appId: "1:333307118863:web:bff47f4f3dcc332b71aa7a",
+    measurementId: "G-STYMF0MNRF"
+  };
